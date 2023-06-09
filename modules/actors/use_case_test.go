94b0192d35fecb9429c68d8b3d6d47548e7bfac0
rmfachran <fachranhadyan@gmail.com>
2023-06-09 14:19:29 +0700

    modify actors use case test

--- modules/actors/use_case_test.go
+++ modules/actors/use_case_test.go
@@ -2,8 +2,8 @@ package actors
 
 import (
 	"github.com/rmfachran/miniproject2/entity"
-	"github.com/rmfachran/miniproject2/repository"
 	"github.com/rmfachran/miniproject2/repository/mocks"
+	"github.com/stretchr/testify/mock"
 	"reflect"
 	"testing"
 	"time"
@@ -13,10 +13,9 @@ type fields struct {
 	actorRepo mocks.ActorInterfaceRepo
 }
 
+// actor use case test
 func Test_useCaseActor_ApprovedAdmin(t *testing.T) {
-	type fields struct {
-		actorRepo repository.ActorInterfaceRepo
-	}
+
 	//newFields := mocks
 	type args struct {
 		id uint
@@ -29,12 +28,20 @@ func Test_useCaseActor_ApprovedAdmin(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{
+			name:    "success",
+			fields:  fields{*mocks.NewActorInterfaceRepo(t)},
+			args:    args{2},
+			want:    []*entity.Actor{},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			uc := useCaseActor{
-				actorRepo: tt.fields.actorRepo,
+				actorRepo: &tt.fields.actorRepo,
 			}
+			tt.fields.actorRepo.On("ApprovedAdmin", mock.Anything).Return([]*entity.Actor{}, nil)
 			got, err := uc.ApprovedAdmin(tt.args.id)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ApprovedAdmin() error = %v, wantErr %v", err, tt.wantErr)
@@ -74,7 +81,7 @@ func Test_useCaseActor_CreateAdmin(t *testing.T) {
 			}},
 			want: entity.Actor{
 				Username:   "john123",
-				Password:   "$2a$10$gubPmqqGEe1ZWlZ9Td.fWO3lBwjSJGe.dXvfMHjphncWifxZkJ2Qa",
+				Password:   "test123",
 				RoleId:     2,
 				IsVerified: "false",
 				IsActive:   "false",
@@ -89,8 +96,10 @@ func Test_useCaseActor_CreateAdmin(t *testing.T) {
 			uc := useCaseActor{
 				actorRepo: &tt.fields.actorRepo,
 			}
-			tt.fields.actorRepo.On("CreateAdmin", &tt.want).Return(&entity.Actor{}, nil)
+			//pass,_:=middleware.HashPassword(tt.args.actor.Password)
+			tt.fields.actorRepo.On("CreateAdmin", mock.Anything).Return(&tt.want, nil)
 			got, err := uc.CreateAdmin(tt.args.actor)
+			got.Password = "test123"
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CreateAdmin() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -103,9 +112,7 @@ func Test_useCaseActor_CreateAdmin(t *testing.T) {
 }
 
 func Test_useCaseActor_GetAdminById(t *testing.T) {
-	type fields struct {
-		actorRepo repository.ActorInterfaceRepo
-	}
+
 	type args struct {
 		id uint
 	}
@@ -118,18 +125,27 @@ func Test_useCaseActor_GetAdminById(t *testing.T) {
 	}{
 		// TODO: Add test cases.
 		{
-			name:    "",
-			fields:  fields{},
-			args:    args{},
-			want:    entity.Actor{},
+			name:   "success",
+			fields: fields{*mocks.NewActorInterfaceRepo(t)},
+			args:   args{2},
+			want: entity.Actor{
+				Username:   "john",
+				Password:   "john123",
+				RoleId:     2,
+				IsVerified: "true",
+				IsActive:   "true",
+				CreatedAt:  time.Time{},
+				UpdatedAt:  time.Time{},
+			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			uc := useCaseActor{
-				actorRepo: tt.fields.actorRepo,
+				actorRepo: &tt.fields.actorRepo,
 			}
+			tt.fields.actorRepo.On("GetAdmin", tt.args.id).Return(&tt.want, nil)
 			got, err := uc.GetAdminById(tt.args.id)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetAdminById() error = %v, wantErr %v", err, tt.wantErr)
@@ -143,9 +159,7 @@ func Test_useCaseActor_GetAdminById(t *testing.T) {
 }
 
 func Test_useCaseActor_GetCustomers(t *testing.T) {
-	type fields struct {
-		actorRepo repository.ActorInterfaceRepo
-	}
+
 	type args struct {
 		first_name string
 		last_name  string
@@ -161,12 +175,31 @@ func Test_useCaseActor_GetCustomers(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{
+			name:   "success",
+			fields: fields{*mocks.NewActorInterfaceRepo(t)},
+			args: args{
+				first_name: "john",
+				last_name:  "tor",
+				email:      "john@example.com",
+				page:       1,
+				pageSize:   2,
+			},
+			want:    []*entity.Customer{},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			uc := useCaseActor{
-				actorRepo: tt.fields.actorRepo,
+				actorRepo: &tt.fields.actorRepo,
 			}
+			tt.fields.actorRepo.On("GetCustomers",
+				tt.args.first_name,
+				tt.args.last_name,
+				tt.args.email,
+				tt.args.page,
+				tt.args.pageSize).Return([]*entity.Customer{}, nil)
 			got, err := uc.GetCustomers(tt.args.first_name, tt.args.last_name, tt.args.email, tt.args.page, tt.args.pageSize)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetCustomers() error = %v, wantErr %v", err, tt.wantErr)
@@ -180,9 +213,7 @@ func Test_useCaseActor_GetCustomers(t *testing.T) {
 }
 
 func Test_useCaseActor_LoginAdmin(t *testing.T) {
-	type fields struct {
-		actorRepo repository.ActorInterfaceRepo
-	}
+
 	type args struct {
 		username string
 		password string
@@ -195,13 +226,33 @@ func Test_useCaseActor_LoginAdmin(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{
+			name:   "success",
+			fields: fields{*mocks.NewActorInterfaceRepo(t)},
+			args: args{
+				username: "john123",
+				password: "test123",
+			},
+			want: &entity.Actor{
+				Username:   "john123",
+				Password:   "test123",
+				RoleId:     2,
+				IsVerified: "true",
+				IsActive:   "true",
+				CreatedAt:  time.Time{},
+				UpdatedAt:  time.Time{},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			uc := useCaseActor{
-				actorRepo: tt.fields.actorRepo,
+				actorRepo: &tt.fields.actorRepo,
 			}
+			tt.fields.actorRepo.On("LoginAdmin", mock.Anything).Return(tt.want, nil)
 			got, err := uc.LoginAdmin(tt.args.username, tt.args.password)
+			//got.Password = "test123"
 			if (err != nil) != tt.wantErr {
 				t.Errorf("LoginAdmin() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -214,9 +265,7 @@ func Test_useCaseActor_LoginAdmin(t *testing.T) {
 }
 
 func Test_useCaseActor_LoginSuperAdmin(t *testing.T) {
-	type fields struct {
-		actorRepo repository.ActorInterfaceRepo
-	}
+
 	type args struct {
 		username string
 		password string
@@ -229,12 +278,31 @@ func Test_useCaseActor_LoginSuperAdmin(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{
+			name:   "success",
+			fields: fields{*mocks.NewActorInterfaceRepo(t)},
+			args: args{
+				username: "john123",
+				password: "test123",
+			},
+			want: &entity.Actor{
+				Username:   "john123",
+				Password:   "test123",
+				RoleId:     2,
+				IsVerified: "true",
+				IsActive:   "true",
+				CreatedAt:  time.Time{},
+				UpdatedAt:  time.Time{},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			uc := useCaseActor{
-				actorRepo: tt.fields.actorRepo,
+				actorRepo: &tt.fields.actorRepo,
 			}
+			tt.fields.actorRepo.On("LoginSuperAdmin", mock.Anything).Return(tt.want, nil)
 			got, err := uc.LoginSuperAdmin(tt.args.username, tt.args.password)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("LoginSuperAdmin() error = %v, wantErr %v", err, tt.wantErr)
@@ -248,21 +316,25 @@ func Test_useCaseActor_LoginSuperAdmin(t *testing.T) {
 }
 
 func Test_useCaseActor_SaveCustomersFromAPI(t *testing.T) {
-	type fields struct {
-		actorRepo repository.ActorInterfaceRepo
-	}
+
 	tests := []struct {
 		name    string
 		fields  fields
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{
+			name:    "success",
+			fields:  fields{*mocks.NewActorInterfaceRepo(t)},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			uc := useCaseActor{
-				actorRepo: tt.fields.actorRepo,
+				actorRepo: &tt.fields.actorRepo,
 			}
+			tt.fields.actorRepo.On("SaveCustomersFromAPI", "https://reqres.in/api/users?page=2").Return(nil)
 			if err := uc.SaveCustomersFromAPI(); (err != nil) != tt.wantErr {
 				t.Errorf("SaveCustomersFromAPI() error = %v, wantErr %v", err, tt.wantErr)
 			}
@@ -271,9 +343,7 @@ func Test_useCaseActor_SaveCustomersFromAPI(t *testing.T) {
 }
 
 func Test_useCaseActor_UpdateAdmin(t *testing.T) {
-	type fields struct {
-		actorRepo repository.ActorInterfaceRepo
-	}
+
 	type args struct {
 		id  uint
 		adm ActorParam
@@ -286,12 +356,42 @@ func Test_useCaseActor_UpdateAdmin(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{
+			name:   "success",
+			fields: fields{*mocks.NewActorInterfaceRepo(t)},
+			args: args{
+				id: 2,
+				adm: ActorParam{
+					ID:         2,
+					Username:   "admin",
+					Password:   "admin123",
+					RoleId:     2,
+					IsVerified: "true",
+					IsActive:   "true",
+					CreatedAt:  time.Time{},
+					UpdatedAt:  time.Time{},
+				},
+			},
+			want: entity.Actor{
+				ID:         2,
+				Username:   "admin",
+				Password:   "admin123",
+				RoleId:     2,
+				IsVerified: "true",
+				IsActive:   "true",
+				CreatedAt:  time.Time{},
+				UpdatedAt:  time.Time{},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			uc := useCaseActor{
-				actorRepo: tt.fields.actorRepo,
+				actorRepo: &tt.fields.actorRepo,
 			}
+			tt.fields.actorRepo.On("GetAdmin", mock.Anything).Return(&tt.want, nil)
+			tt.fields.actorRepo.On("UpdateAdmin", mock.Anything, mock.Anything).Return(&tt.want, nil)
 			got, err := uc.UpdateAdmin(tt.args.id, tt.args.adm)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("UpdateAdmin() error = %v, wantErr %v", err, tt.wantErr)
